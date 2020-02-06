46568d2688fdb9f1408ba5af160b72eb97044733
Mehul Pillai <mehulpillai30@gmail.com>
2020-02-05 19:42:15 -0500

    Solves S2 2018

--- CCC18.java
+++ CCC18.java
@@ -18,9 +18,56 @@ public class CCC18 {
             min = distance < min ? distance : min;
         }
 
-        return Math.floor(min * 10.0) / 10.0;
+        return min;// Math.floor(min * 10.0) / 10.0;
     }
     // PROBLEM 1 end
+
+    // PROBLEM 2
+    // rotate and check if valid
+    void S2(int[][] sunFlower) {
+        while (true) {
+            if (checkValid(sunFlower)) {
+                break;
+            }
+            rotate(sunFlower);
+        }
+
+        System.out.println("3) ");
+        for (int i = 0; i < sunFlower.length; i++) {
+            if (i != 0)
+                System.out.println();
+
+            for (int j = 0; j < sunFlower.length; j++) {
+                System.out.print(j == sunFlower.length - 1 ? sunFlower[i][j] : sunFlower[i][j] + " ");
+            }
+        }
+    }
+
+    void rotate(int[][] sunFlower) {
+        int N = sunFlower.length;
+        int[][] temp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            temp[i] = Arrays.copyOf(sunFlower[i], N);
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0, j2 = N - 1; j < N && j2 >= 0; j++, j2--) {
+                sunFlower[i][j] = temp[j2][i];
+            }
+        }
+    }
+
+    boolean checkValid(int[][] sunFlower) {
+        for (int i = 0; i < sunFlower.length - 1; i++) {
+            for (int j = 0; j < sunFlower.length - 1; j++) {
+                if (sunFlower[i][j] > sunFlower[i][j + 1] || sunFlower[i][j] > sunFlower[i + 1][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -30,8 +77,18 @@ public class CCC18 {
         for (int i = 0; i < N; i++) {
             villages[i] = Integer.parseInt(reader.readLine().trim());
         }
-
         System.out.print("1) ");
         System.out.printf("%.1f\n", problems.S1(villages));
+
+        N = Integer.parseInt(reader.readLine().trim());
+        int[][] sunFlower = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String[] line = reader.readLine().trim().split(" ");
+            for (int j = 0; j < N; j++) {
+                sunFlower[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        problems.S2(sunFlower);
     }
 }
--- DepthFirst.java
+++ DepthFirst.java
@@ -1,4 +1,37 @@
 public class DepthFirst {
+    boolean checkPrime(int num) {
+        for (int i = 2; i <= num / 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void sieveOfEratosthenes(int n) {
+        // Create a boolean array "prime[0..n]" and initialize
+        // all entries it as true. A value in prime[i] will
+        // finally be false if i is Not a prime, else true.
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i < n; i++)
+            prime[i] = true;
+
+        for (int p = 2; p * p <= n; p++) {
+            // If prime[p] is not changed, then it is a prime
+            if (prime[p] == true) {
+                // Update all multiples of p
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+
+        // Print all prime numbers
+        for (int i = 2; i <= n; i++) {
+            if (prime[i] == true)
+                System.out.print(i + " ");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(containsPath());
